949fba5aeb789da067b4b40d5559ac45ca221190
steelcowboy <jhdoubleoseven@gmail.com>
2015-08-03 08:38:51 -0700

    Made script exit if playlist.py returns nothing

--- i3/panel/playlist.sh
+++ i3/panel/playlist.sh
@@ -3,7 +3,10 @@
 output=$(playlist.py)
 lines=$[$(echo "$output" | wc -l)-1]
 
-echo "$output" | dzen2 -fn 'xft:Droid Sans:size=10' -fg '#F6F9FF' -bg '#FF2C00' -x \
-"85" -y "30" -w "230"  -l \
-$lines -e 'onstart=uncollapse;button1=exit;button3=exit' -p 5 -ta l
-
+if [ -z $output ]; then
+	exit 0
+else
+	echo "$output" | dzen2 -fn 'xft:Droid Sans:size=10' -fg '#F6F9FF' -bg '#FF2C00' -x \
+	"85" -y "30" -w "230"  -l \
+	$lines -e 'onstart=uncollapse;button1=exit;button3=exit' -p 5 -ta l
+fi
