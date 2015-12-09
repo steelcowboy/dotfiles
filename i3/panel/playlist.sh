34a1ffdb80d03b7216128770d84561eb2dcc4bfb
steelcowboy <jhdoubleoseven@gmail.com>
2015-12-09 09:45:49 -0800

    Setup my interface to rotate, among a few other things

--- i3/panel/playlist.sh
+++ i3/panel/playlist.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+tod=`cat $HOME/.last_time_state`
+. panel_colors_$tod
+
 output=$(playlist.py)
 lines=$[$(echo "$output" | wc -l)-1]
 
 if [ -z $output ]; then
 	exit 0
 else
-	echo "$output" | dzen2 -fn 'xft:Droid Sans:size=10' -fg '#F6F9FF' -bg '#FF2C00' -x \
+	echo "$output" | dzen2 -fn 'xft:Droid Sans:size=10' -fg \#$color_sec_fg -bg \#$color_sec_bg -x \
 	"85" -y "30" -w "230"  -l \
 	$lines -e 'onstart=uncollapse;button1=exit;button3=exit' -p 5 -ta l
 fi
